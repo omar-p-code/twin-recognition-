10b0ac5fc6f761dbd33f9a0fe1ede922d587a0b6
omar <omaraldyb2020@gmail.com>
2026-04-13 13:35:24 +0200

    MVP Version

--- config.py
+++ config.py
@@ -1,24 +1,26 @@
+import os
 import torch
 
 # ================== Base Directory ==================
-BASE_DIR = r"Y:\dev\myProjects\twin-recognition"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # ================== Paths ==================
-DATA_DIR = f"{BASE_DIR}/data"
-CHECKPOINT_DIR = f"{BASE_DIR}/checkpoints"
+DATA_DIR = os.path.join(BASE_DIR, "data")
+CHECKPOINT_DIR = os.path.join(BASE_DIR, "checkpoints")
+
+# create checkpoints folder automatically
+os.makedirs(CHECKPOINT_DIR, exist_ok=True)
 
 # ================== Hyperparameters ==================
 IMG_SIZE = 224
 BATCH_SIZE = 32
-NUM_EPOCHS = 5
+NUM_EPOCHS = 20
 LEARNING_RATE = 0.0001
 MARGIN = 2.0
-THRESHOLD = 0.4  # Distance below this → same person (Twin)
+THRESHOLD = 0.4
 
 # ================== Device ==================
-DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # ================== Dataset Notes ==================
-# Dataset structure example:
 # data/train/person1/img1.jpg
-# data/train/person2/img2.jpg
